c9b2199af3bad6cf4ccfb55f8b15d81dbd4a41ce
Davide Giuntoli <davide.g@myvirtualab.it>
2023-06-05 15:01:38 +0200

    Inizio costruzione link in php

--- Home_page_2.php
+++ Home_page_2.php
@@ -262,10 +262,10 @@ echo json_encode($data->progetti_in_evidenza);
             <section id="progetti-in-evidenza" style="padding-top: 10px">
                 <ul>
 
-                <?php
-                foreach ($data->progetti_in_evidenza as $progetto) {
-                    echo "<li class='progetto-in-evidenza'>".
-                        "<div class='contenuto-progetto'>
+                    <?php
+                    foreach ($data->progetti_in_evidenza as $progetto) {
+                        echo "<li class='progetto-in-evidenza'>" .
+                            "<div class='contenuto-progetto'>
                             <div>
                                 <p class='sottotitolo-progetto'>Progetto in evidenza</p>
                                 <h3 class='titolo-progetto'>
@@ -280,12 +280,12 @@ echo json_encode($data->progetti_in_evidenza);
                         <div class='immagine-progetto'>
                             <a target='_blank' href='" . $progetto->href . "'>
                                 <div style='max-width: 700px; display: block;'>
-                                    <img alt='". $progetto->banner->alt . "' src='". $progetto->banner->src . "'/>
+                                    <img alt='" . $progetto->banner->alt . "' src='" . $progetto->banner->src . "'/>
                                 </div>
                             </a>
                         </div>";
-                }
-                ?>
+                    }
+                    ?>
                     <!--
                     <li class="progetto-in-evidenza">
                         <div class="contenuto-progetto">
@@ -382,6 +382,36 @@ echo json_encode($data->progetti_in_evidenza);
                                         </svg>
                                     </div>
                                     <div class="lista-link">
+
+
+                                        <?php
+                                        foreach ($data->progetti_secondari as $progetto) {
+                                            if ($progetto->tipo == "GitHub") {
+                                                echo "<a class=\"link\">
+                                                        <svg xmlns=\"http://www.w3.org/2000/svg\" role=\"img\" viewBox=\"0 0 24 24\"
+                                                             fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\"
+                                                             stroke-linecap=\"round\"
+                                                             stroke-linejoin=\"round\" class=\"feather feather-github\">
+                                                            <title>GitHub</title>
+                                                            <path d=\"M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22\"></path>
+                                                        </svg>
+                                                    </a>";
+                                            }
+                                            if ($progetto->tipo == "Link esterno") {
+                                                echo "<a class=\"link\" href=\"Progetto_singolo.html\">
+                                                        <svg xmlns=\"http://www.w3.org/2000/svg\" role=\"img\" viewBox=\"0 0 24 24\"
+                                                             fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\"
+                                                             stroke-linecap=\"round\"
+                                                             stroke-linejoin=\"round\" class=\"feather feather-external-link\">
+                                                            <title>Link esterno</title>
+                                                            <path d=\"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6\"></path>
+                                                            <polyline points=\"15 3 21 3 21 9\"></polyline>
+                                                            <line x1=\"10\" y1=\"14\" x2=\"21\" y2=\"3\"></line>
+                                                        </svg>
+                                                    </a>";
+                                            }
+                                        }
+                                        ?>
                                         <a class="link">
                                             <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24"
                                                  fill="none" stroke="currentColor" stroke-width="2"
