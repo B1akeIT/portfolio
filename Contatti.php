beb78222d72855950f5caa7d7c8b05d62e31b16d
Davide Giuntoli <davide.g@upthere.it>
2023-08-08 19:28:19 +0200

    Inizio fix dei contatti

--- Contatti.php
+++ Contatti.php
@@ -4,6 +4,9 @@ $json = file_get_contents('dati.json');
 $data = json_decode($json);
 $servizio = new Servizio();
 
+$nome = '';
+$email = '';
+$messaggio = '';
 ?>
 
 <!DOCTYPE html>
@@ -66,44 +69,43 @@ $servizio = new Servizio();
                     <form>
                         <div class="form-nome" style="margin-bottom: 10px">
                             <label for="nome">
-                                <span>Nome</span>
+                                <span>Nome *</span>
                             </label>
                             <div class="input-container">
                                 <div class="icona-nome">
-                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                         style="width: 1.25rem; height: 1.25rem; padding-left: 12px"
-                                         viewBox="0 0 24 24" fill="#495670">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#495670">
                                         <path d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"></path>
                                     </svg>
                                 </div>
-                                <input id="nome" style="padding: 0.625rem 2.5rem;" type="text"/>
+                                <input placeholder="Nome" id="nome" name="nome" type="text" required maxlength="50"
+                                       value="<?php echo $nome ?>" style="padding: 0.625rem 2.5rem;"/>
                             </div>
                         </div>
                         <div class="form-mail" style="margin-bottom: 10px; margin-top: 10px">
                             <label for="mail">
-                                <span>Email</span>
+                                <span>Email *</span>
                             </label>
                             <div class="input-container">
                                 <div class="icona-mail">
-                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                         style="width: 1.5rem; height: 1.5rem; padding-left: 12px"
-                                         viewBox="0 0 24 24" fill="#495670">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#495670">
                                         <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
                                         <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
                                     </svg>
                                 </div>
-                                <input id="mail" style="padding: 0.625rem 2.5rem;" type="email"/>
+                                <input placeholder="Email" id="mail" name="email" type="text" required
+                                       value="<?php echo $email ?>"/>
                             </div>
                         </div>
                         <div class="form-messaggio" style="margin-top: 10px">
                             <label for="messaggio">
-                                <span>Messaggio</span>
+                                <span>Messaggio *</span>
                             </label>
                             <div class="input-container">
-                                <textarea id="messaggio" rows="6" style="padding: 0.625rem 12px;"></textarea>
+                                <textarea placeholder="Messaggio" id="messaggio" name="messaggio" rows="6" required><?php echo $messaggio ?></textarea>
                             </div>
                         </div>
-                        <button type="submit" class="button-invia-email">
+                        <input type="hidden" name="inviato" value="1">
+                        <button type="button" class="button-invia-email">
                             Invia!
                         </button>
                     </form>
--- servizio/Servizio.php
+++ servizio/Servizio.php
@@ -19,5 +19,39 @@ class Servizio
         $this->customFooter = new CustomFooter();
     }
 
+    public function inviaDati($nomeFile, $nome = '', $email = '', $messaggio = '') {
+        $stringa = "";
+        $stringa .= "Nome: ";
+        $stringa .= $nome;
+        $stringa .= "Email: ";
+        $stringa .= $email;
+        $stringa .= "Messaggio: ";
+        $stringa .= $messaggio;
+
+        $this->scriviTesto($nomeFile, $stringa);
+    }
+
+    // "<script>console.log({stringa});</script>" permette di usare la console consultabile dal browser.
+    public function scriviTesto($nomeFile, $testoDaScrivere)
+    {
+        echo "testoDaScrivere " . $testoDaScrivere;
+        $successo = null;
+        if (!$fp = fopen($nomeFile, 'a')) {
+            echo "Errore: Non posso aprire il file " . $nomeFile;
+        } else {
+            if (!is_writable($nomeFile)) {
+                echo "Errore: Non posso scrivere nel file " . $nomeFile;
+            } else {
+                file_put_contents($nomeFile, "");
+                if (!fwrite($fp, $testoDaScrivere)) {
+                    echo "Errore: Errore nella scrittura nel file " . $nomeFile;
+                } else {
+                    $successo = true;
+                }
+            }
+        }
+        fclose($fp);
+        return $successo;
+    }
 
 }
